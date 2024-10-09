9584468ffe606843effb9d919d8255394b2241b4
graciela23garcia <goodtobegrace23@gmail.com>
2024-10-09 12:36:07 -0700

    Updates

---
+++ precache-manifest.64282a3c306aba81f3fb2e81a7299a0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93996ac7033351ba638d4f69bb5888c8",
+    "url": "/cooperspalmer/index.html"
+  },
+  {
+    "revision": "86c0fd0d8bf341548aac",
+    "url": "/cooperspalmer/static/css/2.27d55f80.chunk.css"
+  },
+  {
+    "revision": "4d5e38773fc83a1f3101",
+    "url": "/cooperspalmer/static/css/main.38a4c9c3.chunk.css"
+  },
+  {
+    "revision": "86c0fd0d8bf341548aac",
+    "url": "/cooperspalmer/static/js/2.08ee7934.chunk.js"
+  },
+  {
+    "revision": "4d5e38773fc83a1f3101",
+    "url": "/cooperspalmer/static/js/main.98ec7b01.chunk.js"
+  },
+  {
+    "revision": "24e3430d064a0cdbce43",
+    "url": "/cooperspalmer/static/js/runtime~main.66bda6c6.js"
+  }
+]);
